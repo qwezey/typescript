fa96c9b40ee12a5870689c58c521b33e2ef5f8f7
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2025-10-14 22:49:46 +0000

    add prettier and eslint config

---
+++ eslint.config.js
@@ -0,0 +1,3 @@
+import {qwezeyEslintConfig} from './dist/index.js';
+
+export default qwezeyEslintConfig;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import type { Config } from "prettier";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import importPlugin from "eslint-plugin-import";
-import { defineConfig } from "eslint/config";
+import type {Config} from 'prettier';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import importPlugin from 'eslint-plugin-import';
+import {defineConfig} from 'eslint/config';
 
 export const qwezeyEslintConfig = defineConfig(
   eslint.configs.recommended,
@@ -11,25 +11,25 @@ export const qwezeyEslintConfig = defineConfig(
   tseslint.configs.stylisticTypeChecked,
   importPlugin.flatConfigs.recommended,
   importPlugin.flatConfigs.typescript,
-  { languageOptions: { parserOptions: { projectService: true } } },
+  {languageOptions: {parserOptions: {projectService: true}}},
   {
     rules: {
-      "import/no-extraneous-dependencies": "error",
-      "import/no-unresolved": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-confusing-void-expression": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/restrict-template-expressions": "off",
-      "no-warning-comments": ["warn", { terms: ["todo"], location: "start" }],
+      'import/no-extraneous-dependencies': 'error',
+      'import/no-unresolved': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-confusing-void-expression': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'no-warning-comments': ['warn', {terms: ['todo'], location: 'start'}],
     },
   },
-  eslintPluginPrettier
+  eslintPluginPrettier,
 );
 
 export const qwezeyPrettierConfig: Config = {
   bracketSpacing: false,
   singleQuote: true,
-  trailingComma: "all",
-  arrowParens: "avoid",
-  objectWrap: "collapse",
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  objectWrap: 'collapse',
 };
